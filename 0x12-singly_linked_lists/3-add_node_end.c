45bad126c83bb35d101d821881c1ea992f070899
stephen_adah <mailstephenreal@gmail.com>
2023-07-28 19:19:31 -0700

    3. Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,30 +16,38 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *current_node;
 	size_t n;
-
+	/*alloction of memory size to new_node*/
 	new_node = malloc(sizeof(list_t));
+	/*check if memory allocation was successful */
 	if (new_node == NULL)
 		return (NULL);
-
+	/*duplicate string literal to new_node*/
 	new_node->str = strdup(str);
 
-
+	/* for loop to get the size of size_t n */
 	for (n = 0; str[n]; n++)
-		;
+		; /* semi colon here is to take no action by the looping*/
 
-	new_node->len = n;
-	new_node->next = NULL;
-	current_node = *head;
-
-	if (current_node == NULL)
+	new_node->len = n; /* assigned n to len*/
+	new_node->next = NULL; 
+	/*[above line] since new node just created, pointer address is NULL*/
+	current_node = *head; 
+	/* [above line]assigned current_node to the null address in *head */
+	/* at the above line both new_node and current_node pointer address are NULL */
+	if (current_node == NULL) /* this condition is true*/
 	{
-		*head = new_node;
-	}
+		*head = new_node; /*new_node address is assigned to head */
+	} /* current_node is left with NULL address*/
 	else
 	{
-		while (current_node->next != NULL)
-			current_node = current_node->next;
-		current_node->next = new_node;
+		while (current_node->next != NULL) /*this condition is true because*/
+		       /*current_node will always come with a NUL address*/
+			current_node = current_node->next; 
+		/* current_node will be assigned*/
+		/*with the address of the subsequent next current_node*/
+		current_node->next = new_node; 
+		/*[above line] the subsequent current_node will be assigned*/
+		/*with the NULL address assigned to new_node*/
 	}
-	return (*head);
+	return (*head); /*return head that carries the address of the first node in series*/
 }
