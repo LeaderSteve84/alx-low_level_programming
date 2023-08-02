6c6fa1da7446a99dcca02bb9e4bc27dea51cb4c7
stephen_adah <mailstephenreal@gmail.com>
2023-08-02 00:21:22 -0700

    %. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,17 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *temp;
+	listint_t *temp;
 
-	if (*head == NULL)
+	/*check if head is empty*/
+	if (head == NULL)
 		return;
-
-	current = *head;
-	while (current != NULL)
+	/*initiate iteration loop*/
+	while (*head)
 	{
-		temp = current;
-		current = current->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	*head = NULL;
+	*head = NULL;/*set head to NULL*/
 }
