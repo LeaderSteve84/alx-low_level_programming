4a3e3f532c58ff02fe74334177e9b501338ee415
stephen_adah <mailstephenreal@gmail.com>
2023-09-14 14:17:28 -0700

    2. Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
-		printf("memory allocation failed\n");
 		return (NULL);
 	}
 	else
